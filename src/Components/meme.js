5ae2846c900d07f46226121a0a780a012b9b3efe
Raul Mansur <raulfmansur@hotmail.com>
2018-07-10 14:34:12 -0300

    cards added

--- src/Components/meme.js
+++ src/Components/meme.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react'
 class Meme extends Component {
 	render() {
 		return (
-			<div style={{float: 'left'}}>
-				<p>{this.props.meme.name}</p>
-				<img style={{width: '250px', height: '250px'}}src={this.props.meme.url}/>
-			</div>
-			)
+			<div class="row" style={{float: 'left'}}>
+			    <div class="col s12 m7">
+			      <div style={{width: '250px', height: '250px'}} class="card">
+			        <div class="card-image">
+			          <img style={{width: '250px', height: '250px'}} src={this.props.meme.url}/>
+			          <span class="card-title">{this.props.meme.name}</span>
+			        </div>
+			      </div>
+			    </div>
+			  </div>
+		)
 	}
 }
 
