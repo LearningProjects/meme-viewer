668c3fc653edcd35317f5851d4f376842303676d
Gustavo de Paula <gustavodepaula@disroot.org>
2018-07-15 18:44:37 -0300

    feat: readable subtitle and download btn

--- src/Components/meme.js
+++ src/Components/meme.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
 
 class Meme extends Component {
-	render() {
-		return (
-			<div class="row" style={{float: 'left'}}>
-			    <div class="col s12 m7">
-			      <div style={{width: '250px', height: '250px'}} class="card">
-			        <div class="card-image">
-			          <img style={{width: '250px', height: '250px'}} src={this.props.meme.url}/>
-			          <span class="card-title">{this.props.meme.name}</span>
-			        </div>
-			      </div>
-			    </div>
-			  </div>
-		)
-	}
+  render () {
+    return (
+      <div className='row' style={{float: 'left'}}>
+        <div className='col s12 m7'>
+          <div style={{width: '250px', height: '350px'}} className='card'>
+            <div className='card-image'>
+              <img style={{width: '250px', height: '250px'}} src={this.props.meme.url} />
+              <a className='btn-floating halfway-fab waves-effect waves-light red' href={this.props.meme.url} download>
+                <i className='material-icons'>file_download</i>
+              </a>
+            </div>
+            <div className='card-content'>
+              <span className='card-title truncate'>{this.props.meme.name}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Meme
