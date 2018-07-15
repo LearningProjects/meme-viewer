8e077db95b52c1ba6c2c90e042283b3dcdd3cdfc
Gustavo de Paula <gustavodepaula@disroot.org>
2018-07-15 18:44:22 -0300

    refac: lint & row

--- src/Components/meme_show.js
+++ src/Components/meme_show.js
@@ -3,23 +3,28 @@ import Meme from './meme.js'
 import axios from 'axios';
 
 class MemeShow extends Component {
-	state = {
-		memesList: []
-	}
+  state = {
+    memesList: []
+  }
+  
+  componentDidMount () {
+    axios.get(`https://api.imgflip.com/get_memes`)
+      .then(res => {
+        this.setState({memesList: res.data.data.memes})
+      })
+  }
 
-	componentDidMount() {
-		axios.get(`https://api.imgflip.com/get_memes`)
-		.then(res => {
-			this.setState({memesList: res.data.data.memes})
-		})
-			}
-	render() {
-		return (
-			<div className="container">
-				{this.state.memesList.map(data => <Meme meme={data}></Meme>)}
-			</div>
-			)
-	}
+  render () {
+    return (
+      <div className='container'>
+        <div className='section'>
+          <div className='row' >
+            {this.state.memesList.map(data => <Meme meme={data}></Meme>)}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default MemeShow
